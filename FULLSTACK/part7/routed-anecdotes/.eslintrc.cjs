4cf77e73d11adbe688dfc989f14a0f31d0bad430
AA3977 <aa3977@student.jamk.fi>
2023-11-01 13:31:19 +0200

    Update FULLSTACK part7 with exercises 7.1-7.2 done

--- FULLSTACK/part7/routed-anecdotes/.eslintrc.cjs
+++ FULLSTACK/part7/routed-anecdotes/.eslintrc.cjs
@@ -12,9 +12,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react/jsx-uses-react": "error",   
+    "react/jsx-uses-vars": "error",
+    "react/prop-types": "off"
   },
 }
--- FULLSTACK/part7/routed-anecdotes/src/App.jsx
+++ FULLSTACK/part7/routed-anecdotes/src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useMatch
+} from 'react-router-dom'
 
+
+//COMPONENTS
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">Anecdotes</Link>
+      <Link style={padding} to="/create">Create New</Link>
+      <Link style={padding} to="/about">About</Link>
+    </div>
+  )
+}
+
+const Anecdote = ({ anecdote }) => {
+  const margins = {
+    marginBottom: 5
+  }
+
+  return (
+    <div>
+      <h3>{anecdote.content}</h3>
+      <div style={margins}>Votes: {anecdote.votes}</div>
+      <div style={margins}>For more info: <a href="url">{anecdote.info}</a></div>
     </div>
   )
 }
@@ -17,7 +40,10 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)}
     </ul>
   </div>
 )
@@ -30,19 +56,26 @@ const About = () => (
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      An anecdote is a story with a point.</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
 )
 
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+const Footer = () => {
+  const margins = {
+    marginTop: 25
+  }
 
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
+  return (
+    <div style={margins}>
+      Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+
+      See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    </div>
+  )
+  
+}
 
 const CreateNew = (props) => {
   const [content, setContent] = useState('')
@@ -122,13 +155,24 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : <null></null>
+
   return (
     <div>
       <h1>Software anecdotes</h1>
+
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+
+      <Routes>
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} />} />
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path='/create' element={<CreateNew addNew={addNew} />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+
       <Footer />
     </div>
   )
